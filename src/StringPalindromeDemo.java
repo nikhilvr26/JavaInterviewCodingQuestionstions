ecab7329f41f398f510a24d8061e6b9a57c3e7e8
nikhilvr26 <verma.nikhil2000@gmail.com>
2026-05-06 00:44:25 +0530

    modify the code

--- src/StringPalindromeDemo.java
+++ src/StringPalindromeDemo.java
@@ -3,14 +3,14 @@ public class StringPalindromeDemo {
         str = str.toLowerCase(); // convert string into lower case. So, capital letters will not give any problem if string have that.
         char[] strArray = str.toCharArray(); // storing string into char array, so that we can iterate that.
         for(int i=0; i<strArray.length/2; i++){ // iterating the char array to the half of its length because no need to iterate whole length
-            if(strArray[i]==strArray[strArray.length-1-i]){ // comparing first index with the last index and increasing from the start and decreasing from the last.
-                return true; // if the condition is true the return true
+            if(strArray[i]!=strArray[strArray.length-1-i]){ // comparing first index with the last index and increasing from the start and decreasing from the last.
+                return false; //
             }
         }
-        return false; // else false
+        return true;
     }
     public static void main(String[] args) {
-        String str = "Madam";
+        String str = "Madamm";
         if(isStringPalindrome(str)){
             System.out.println(str+" is Palindrome");
         }else{
